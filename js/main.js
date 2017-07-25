383afb9e68409baa9982763ee3eb6a805293503c
Tiago Moreno Coutinho <tiago.coutinho@otimaideia.com.br>
2017-07-25 15:17:31 -0300

    Parallax mobile e otimizações no script

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
-/*$( document ).ready(function() {
-    $( ".carregando" ).toggleClass('ok');
-    //$("html").easeScroll();
-    $(window).scroll(function() {
-      var l1 = $(window).scrollTop();
-      $('#l1').css('transform',("translateY(" + l1 / 11.4 + 'px)'));
-      var l2 = $(window).scrollTop();
-      $('#l2').css('transform',("translateY(" + l1 / 11.25 + 'px)'));
-      var l3 = $(window).scrollTop();
-      $('#l3').css('transform',("translateY(" + l1 / 3.15 + 'px)'));
-      var l4 = $(window).scrollTop();
-      $('#l4').css('transform',("translateY(" + l1 / 2.15 + 'px)'));
-      var l5 = $(window).scrollTop();
-      $('#l5').css('transform',("translateY(" + l1 / 1.75 + 'px)'));
-      var l6 = $(window).scrollTop();
-      $('#l6').css('transform',("translateY(" + l1 / 1.25 + 'px)'));
-    });
-}); */
 $(document).ready(function(){
-  $("html").easeScroll();
+  if(!is_touch_device()){//Habilita o ease scroll apenas em telas não touch
+    $("html").easeScroll();
+  }
+  else{
+    $(".banner-parallax").addClass('touch');
+  }
   function draw() {
     requestAnimationFrame(draw);
     scrollEvent();
@@ -25,17 +12,17 @@ $(document).ready(function(){
   draw();
 });
 function scrollEvent(){
-  if(!is_touch_device()){
+  //if(!is_touch_device()){
     viewportTop = $(window).scrollTop();
     windowHeight = $(window).height();
     viewportBottom = windowHeight+viewportTop;
     if($(window).width())
-      $('[data-parallax="true"]').each(function(){
-        distance = viewportTop * $(this).attr('data-speed');
-        if($(this).attr('data-direction') === 'up'){ sym = '-'; } else { sym = ''; }
+      $('[prllx="true"]').each(function(){
+        distance = viewportTop * $(this).attr('prllx-speed');
+        if($(this).attr('prllx-up') === 'true'){ sym = '-'; } else { sym = ''; }
           $(this).css('transform','translate3d(0, ' + sym + distance +'px,0)');
         });
-    }
+    //}
 }   
 function is_touch_device() {
   return 'ontouchstart' in window // works on most browsers 
