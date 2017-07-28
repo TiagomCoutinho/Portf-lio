87a91462d577c7e9d3b31b076d34b56b96b80bea
Tiago Moreno Coutinho <tiago.coutinho@otimaideia.com.br>
2017-07-28 16:42:24 -0300

    Adição do script de delay para exibir elemento

--- js/main.js
+++ js/main.js
@@ -7,6 +7,15 @@ $(document).ready(function(){
 	if(!SeePrllxTouch()){
 		$("html").easeScroll();//Habilita o ease scroll apenas em telas não touch
 	}
+  $(window).scroll( function(){
+    $('.show-me').each( function(i){      
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if( bottom_of_window > bottom_of_object ){        
+        $(this).addClass('isee');              
+      }        
+    });   
+  });
 });
 $(document).ready(function(){
   $("a").on('click', function(event) {
@@ -21,7 +30,7 @@ $(document).ready(function(){
     }
   });
 });
-var targetOffset = $("#anchor").offset().top;
+var targetOffset = $("#sobre").offset().top;
 
 var $w = $(window).scroll(function(){
     if ( $w.scrollTop() > targetOffset ) {   
